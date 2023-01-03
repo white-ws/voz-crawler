fbfd7d5363eb50aaac01fe15ef1cf9bd2101dce2
Viet Hoang <white.xyza@gmail.com>
2023-01-03 15:19:43 +0900

    crawl voz

--- scrapper/scrapper/spiders/voz_spider.py
+++ scrapper/scrapper/spiders/voz_spider.py
@@ -13,17 +13,33 @@ class VozSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # results = response.css('article.message-body div.bbWrapper').getall()
         commentSelectors = response.css('article.message-body')
         
-        filename = f'result.html'
+        filename1 = f'result.tsv'
+        filename2 = f'exception.txt'
 
-        # handle bad case
-        with open(filename, 'w') as f:
-            for selector in commentSelectors:
-                stripped = filter(lambda s: s != '\n', selector.css('div.bbWrapper *::text').getall()) 
-                text = '\n'.join(stripped)
-                f.write(text)
-                f.write("\n=====================================================================\n")
-            f.close()
+        
+        with open(filename1, 'a') as f1:
+            with open(filename2, 'a') as f2:
+                for commentSelector in commentSelectors:
+                    if len(commentSelector.css('div ul')) == 0:
+                        # handle bad case
+                        stripped = filter(lambda s: s != '\n', commentSelector.css('div.bbWrapper *::text').getall()) 
+                        text = '\n'.join(stripped)
+                        f2.write(text)
+                        f2.write("\n=====================================================================\n")
+                    else:
+                        # handle legit case
+                        for selector in commentSelector.css('div ul'):
+                            stripped = filter(lambda s: s != '\n', selector.css('li *::text').getall()) 
+                            text = '\t'.join(stripped)
+                            f1.write(text + "\n")
+                f2.close()
+            f1.close()
+        
+        next_page = response.css('a.pageNav-jump--next::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
             
